02b1c4d28c2f6c26459a9d975a17ae56e3484fc5
Soren <vlad1998ss@mail.ru>
2024-03-03 20:13:17 +0300

    check6

--- homework/dmia/classifiers/logistic_regression.py
+++ homework/dmia/classifiers/logistic_regression.py
@@ -5,10 +5,10 @@ import numpy as np
 class LogisticRegression():
 
     def __init__(self):
-        self.thetas = None
+        self.w = None
         self.loss_history = []
 
-    def train(self, x, y, num_iters=1, learning_rate=1e-1, batch_size=200, reg=1e-5, verbose=True):
+    def train(self, x, y, num_iters=1, learning_rate=1e-1, batch_size=200, reg=1e-5, verbose=False):
 
         num_train, dim = x.shape
 
@@ -16,57 +16,58 @@ class LogisticRegression():
         self.append_biases(x)
 
         # инициализирует веса
-        thetas, n = np.random.randn(dim) * 0.01, x.shape[0]
+        w, n = np.random.randn(dim) * 0.01, x.shape[0]
 
         np.random.randn(dim) * 0.01
 
         loss_history = []
 
         for it in range(num_iters):
-            print(it)
+            # print(it)
             for batch_start in range(0, x.shape[0], batch_size):
                 random_indices = np.random.choice(num_train, batch_size)
                 X_batch = x[random_indices]
                 y_batch = y[random_indices]
 
                 # метод сделает прогноз с текущими весами
-                y_pred = self.h(X_batch, thetas)
+                y_pred = self.h(X_batch, w)
 
                 # найдет и запишет уровень ошибки
                 loss = self.objective(y_batch, y_pred)
-                regularization_loss = (reg / (2 * n)) * np.sum(np.square(thetas[1:]))
+                regularization_loss = (reg / (2 * n)) * np.sum(np.square(w[1:]))
                 loss += regularization_loss
                 loss_history.append(loss)
 
                 # рассчитает градиент
                 grad = self.gradient(X_batch, y_batch, y_pred, n)
-                regularization_grad = (reg / n) * thetas
+                regularization_grad = (reg / n) * w
                 regularization_grad[0] = 0
                 grad += regularization_grad
 
                 # и обновит веса
-                thetas -= learning_rate * grad
+                w -= learning_rate * grad
 
             if verbose and it % 100 == 0:
                 print('iteration %d / %d: loss %f' % (it, num_iters, loss))
 
-        self.thetas = thetas
+        self.w = w
         self.loss_history = loss_history
 
     # метод .predict() делает прогноз с помощью обученной модели
     def predict(self, x):
         self.append_biases(x)
 
-        z = np.dot(x.toarray(), self.thetas)
+        z = np.dot(x.toarray(), self.w)
         probs = np.array([self.stable_sigmoid(value) for value in z])
 
-        return np.where(probs >= 0.5, 1, 0), probs
+        return np.where(probs >= 0.5, 1, 0)
+        # return np.where(probs >= 0.5, 1, 0), probs
 
     def append_biases(self, X):
         return sparse.hstack((np.ones(X.shape[0])[:, np.newaxis], X)).tocsr()
 
-    def h(self, x, thetas):
-        z = np.dot(x.toarray(), thetas)
+    def h(self, x, w):
+        z = np.dot(x.toarray(), w)
         return np.array([self.stable_sigmoid(value) for value in z])
 
     def objective(self, y, y_pred):
