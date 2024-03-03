d822ce566492c491c1af941d777189ddf25beaf7
Soren <vlad1998ss@mail.ru>
2024-03-03 18:27:49 +0300

    check5

--- homework/dmia/classifiers/logistic_regression.py
+++ homework/dmia/classifiers/logistic_regression.py
@@ -1,215 +1,84 @@
-import numpy as np
 from scipy import sparse
+import numpy as np
 
 
-# from scipy.sparse import exp as sparse_exp
+class LogisticRegression():
 
-class LogisticRegression:
     def __init__(self):
-        self.w = None
-        self.loss_history = None
-
-    def train(self, X, y, learning_rate=1e-3, reg=1e-5, num_iters=100,
-              batch_size=200, verbose=False):
-        """
-        Train this classifier using stochastic gradient descent.
-
-        Inputs:
-        - X: N x D array of training data. Each training point is a D-dimensional
-             column.
-        - y: 1-dimensional array of length N with labels 0-1, for 2 classes.
-        - learning_rate: (float) learning rate for optimization.
-        - reg: (float) regularization strength.
-        - num_iters: (integer) number of steps to take when optimizing
-        - batch_size: (integer) number of training examples to use at each step.
-        - verbose: (boolean) If true, print progress during optimization.
-
-        Outputs:
-        A list containing the value of the loss function at each training iteration.
-        """
-        # Add a column of ones to X for the bias sake.
-        # X = LogisticRegression.append_biases(X)
-        num_train, dim = X.shape
-        if self.w is None:
-            # lazily initialize weights
-            self.w = np.random.randn(dim) * 0.01  # (dim, )
-
-        # Run stochastic gradient descent to optimize W
+        self.thetas = None
         self.loss_history = []
+
+    def train(self, x, y, num_iters=1, learning_rate=1e-1, batch_size=200, reg=1e-5, verbose=True):
+
+        num_train, dim = x.shape
+
+        # добавит столбец из единиц
+        self.append_biases(x)
+
+        # инициализирует веса
+        thetas, n = np.random.randn(dim) * 0.01, x.shape[0]
+
+        np.random.randn(dim) * 0.01
+
+        loss_history = []
+
         for it in range(num_iters):
-            print(f"iter: {it}")
-            #########################################################################
-            # TODO:                                                                 #
-            # Sample batch_size elements from the training data and their           #
-            # corresponding labels to use in this round of gradient descent.        #
-            # Store the data in X_batch and their corresponding labels in           #
-            # y_batch; after sampling X_batch should have shape (batch_size, dim)   #
-            # and y_batch should have shape (batch_size,)                           #
-            #                                                                       #
-            # Hint: Use np.random.choice to generate indices. Sampling with         #
-            # replacement is faster than sampling without replacement.              #
-            #########################################################################
-            random_indices = np.random.choice(num_train, batch_size)
-            X_batch = X[random_indices]
-            y_batch = y[random_indices]
+            print(it)
+            for batch_start in range(0, x.shape[0], batch_size):
+                random_indices = np.random.choice(num_train, batch_size)
+                X_batch = x[random_indices]
+                y_batch = y[random_indices]
 
-            #########################################################################
-            #                       END OF YOUR CODE                                #
-            #########################################################################
+                # метод сделает прогноз с текущими весами
+                y_pred = self.h(X_batch, thetas)
 
-            # evaluate loss and gradient
-            loss, gradW = self.loss(X_batch, y_batch, reg)
-            self.loss_history.append(loss)
-            # perform parameter update
-            #########################################################################
-            # TODO:                                                                 #
-            # Update the weights using the gradient and the learning rate.          #
-            #########################################################################
-            self.w[:-1] -= learning_rate * gradW
+                # найдет и запишет уровень ошибки
+                loss = self.objective(y_batch, y_pred)
+                regularization_loss = (reg / (2 * n)) * np.sum(np.square(thetas[1:]))
+                loss += regularization_loss
+                loss_history.append(loss)
 
-            #########################################################################
-            #                       END OF YOUR CODE                                #
-            #########################################################################
+                # рассчитает градиент
+                grad = self.gradient(X_batch, y_batch, y_pred, n)
+                regularization_grad = (reg / n) * thetas
+                regularization_grad[0] = 0
+                grad += regularization_grad
+
+                # и обновит веса
+                thetas -= learning_rate * grad
 
             if verbose and it % 100 == 0:
                 print('iteration %d / %d: loss %f' % (it, num_iters, loss))
 
-        return self
+        self.thetas = thetas
+        self.loss_history = loss_history
 
-    @staticmethod
-    def sigmoid(z):
-        return 1 / (1 + np.exp(-(z + 1e-9)))
+    # метод .predict() делает прогноз с помощью обученной модели
+    def predict(self, x):
+        self.append_biases(x)
 
-    def predict_proba(self, X, append_bias=False):
-        """
-        Use the trained weights of this linear classifier to predict probabilities for
-        data points.
+        z = np.dot(x.toarray(), self.thetas)
+        probs = np.array([self.stable_sigmoid(value) for value in z])
 
-        Inputs:
-        - X: N x D array of data. Each row is a D-dimensional point. !(maybe N = batch size)!
-        - append_bias: bool. Whether to append bias before predicting or not.
+        return np.where(probs >= 0.5, 1, 0), probs
 
-        Returns:
-        - y_proba: Probabilities of classes for the data in X. y_pred is a 2-dimensional
-          array with a shape (N, 2), and each row is a distribution of classes [prob_class_0, prob_class_1].
-        """
-        if append_bias:
-            X = LogisticRegression.append_biases(X)
-        ###########################################################################
-        # TODO:                                                                   #
-        # Implement this method. Store the probabilities of classes in y_proba.   #
-        # Hint: It might be helpful to use np.vstack and np.sum                   #
-        ###########################################################################
-        # prob_y1 = 1/(1 + exp(-XB)), где XB = X1B1 + X2B2...XnBn
-        # print(X.shape)
-        # print(self.w.shape)
-        #         prob_class_1 = 1/(1 + np.exp(-1*XB_sum))  # (N,)
-        # Maybe np.dot not suit
-        # XW = np.dot(X, self.w)
-        XW = np.dot(X.toarray(), self.w)
-        # print(type(XW))
-        # print(XW.shape)
-        # print(XW)
-        # print(XW.data)
-        prob_class_1 = self.sigmoid(XW)
-        prob_class_0 = 1 - prob_class_1
+    def append_biases(self, X):
+        return sparse.hstack((np.ones(X.shape[0])[:, np.newaxis], X)).tocsr()
 
-        prob_class_1 = prob_class_1.reshape(-1, 1)  # (N, 1)
-        prob_class_0 = prob_class_0.reshape(-1, 1)
+    def h(self, x, thetas):
+        z = np.dot(x.toarray(), thetas)
+        return np.array([self.stable_sigmoid(value) for value in z])
 
-        y_proba = np.concatenate((prob_class_0, prob_class_1), axis=1)  # (N, 2)
-
-        ###########################################################################
-        #                           END OF YOUR CODE                              #
-        ###########################################################################
-        return y_proba
-
-    def predict(self, X):
-        """
-        Use the ```predict_proba``` method to predict labels for data points.
-
-        Inputs:
-        - X: N x D array of training data. Each column is a D-dimensional point.
-
-        Returns:
-        - y_pred: Predicted labels for the data in X. y_pred is a 1-dimensional
-          array of length N, and each element is an integer giving the predicted
-          class.
-        """
-
-        ###########################################################################
-        # TODO:                                                                   #
-        # Implement this method. Store the predicted labels in y_pred.            #
-        ###########################################################################
-        y_proba = self.predict_proba(X, append_bias=True)
-        y_pred = y_proba.copy()
-        y_pred[y_pred > 0.5] = 1
-        y_pred[y_pred < 0.5] = 0
-        y_pred = y_pred[:, 0]  # (N,)
-
-        ###########################################################################
-        #                           END OF YOUR CODE                              #
-        ###########################################################################
-        return y_pred
-
-    @staticmethod
-    def get_loss_sum(y, y_pred):
-
-        # рассчитаем функцию потерь для y = 1, добавив 1e-9, чтобы избежать ошибки при log(0)
+    def objective(self, y, y_pred):
         y_one_loss = y * np.log(y_pred + 1e-9)
-
-        # также рассчитаем функцию потерь для y = 0
         y_zero_loss = (1 - y) * np.log(1 - y_pred + 1e-9)
+        return -np.mean(y_zero_loss + y_one_loss)
 
-        # сложим и разделим на количество наблюдений
-        return -np.sum(y_zero_loss + y_one_loss)
+    def gradient(self, x, y, y_pred, n):
+        return np.dot(x.toarray().T, (y_pred - y)) / n
 
-    @staticmethod
-    def get_gradient_sum(x, y, y_pred, n):
-        return np.dot(x.toarray().T, (y_pred - y))
-
-    def loss(self, X_batch, y_batch, reg):
-        """Logistic Regression loss function
-        Inputs:
-        - X: N x D array of data. Data are D-dimensional rows
-        - y: 1-dimensional array of length N with labels 0-1, for 2 classes
-        Returns:
-        a tuple of:
-        - loss as single float
-        - gradient with respect to weights w; an array of same shape as w
-        """
-        dw = np.zeros_like(self.w)  # initialize the gradient as zero
-        N = X_batch.shape[0]
-        loss = 0
-        # Compute loss and gradient. Your code should not contain python loops.
-        # *Произвести предсказания и подсчитать ошибки
-
-        y_pred = self.predict(X_batch)
-
-        loss_sum = self.get_loss_sum(y_batch, y_pred)
-
-        gradient_sum = self.get_gradient_sum(X_batch, y_batch, y_pred, N)
-
-        # Right now the loss is a sum over all training examples, but we want it
-        # to be an average instead so we divide by num_train.
-        # Note that the same thing must be done with gradient.
-        loss = loss_sum / N
-        dw = gradient_sum / N
-
-        # Add regularization to the loss and gradient.
-        # Note that you have to exclude bias term in regularization.
-        # theta = np.zeros(X_batch.shape[1])  # Initialize parameter vector
-
-        regularization_term_loss = (reg / (2 * N)) * np.sum(np.square(self.w[:-1]))  # excluding bias term
-        # regularization_term_grad = (reg / N) * np.concatenate(([0], self.w[:-1]))  # excluding bias term
-        regularization_term_grad = (reg / N) * self.w[:-1]  # excluding bias term
-
-
-        loss += regularization_term_loss
-        dw += regularization_term_grad
-
-        return loss, dw
-
-    @staticmethod
-    def append_biases(X):
-        return sparse.hstack((X, np.ones(X.shape[0])[:, np.newaxis])).tocsr()
+    def stable_sigmoid(self, z):
+        if z >= 0:
+            return 1 / (1 + np.exp(-z))
+        else:
+            return np.exp(z) / (np.exp(z) + 1)
--- homework/log_regr_new.py
+++ homework/log_regr_new.py
@@ -1,3 +1,4 @@
+from scipy import sparse
 from scipy.sparse import coo_array, hstack
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
@@ -29,13 +30,7 @@ import pandas as pd
 from sklearn.metrics import accuracy_score, confusion_matrix
 from sys import getsizeof
 
-
-A = np.array([1,2,3,4]).reshape((2,2))
-N = A.shape[1]
-new = np.c_[ A, np.ones(N) ]              # add a column
-
-pass
-
+from dmia.classifiers.logistic_regression import LogisticRegression
 
 
 class LogReg():
@@ -49,7 +44,7 @@ class LogReg():
     # этому методу мы передадим признаки и целевую переменную
     # кроме того, мы зададим значения по умолчанию
     # для количества итераций и скорости обучения
-    def fit(self, x, y, iter=1, learning_rate=1e-3, batch_size=200):
+    def fit(self, x, y, iter=1, learning_rate=1e-1, batch_size=200, reg=1e-5):
 
         # метод создаст "правильные" копии датафрейма
         # x, y = x.copy(), y.copy()
@@ -60,7 +55,10 @@ class LogReg():
         self.add_ones(x)
 
         # инициализирует веса и запишет в переменную n количество наблюдений
-        thetas, n = np.zeros(x.shape[1]), x.shape[0]
+        # thetas, n = np.zeros(x.shape[1]), x.shape[0]
+        thetas, n = np.random.randn(dim) * 0.01, x.shape[0]
+
+        np.random.randn(dim) * 0.01
 
         # создадим список для записи уровня ошибки
         loss_history = []
@@ -68,21 +66,29 @@ class LogReg():
         # в цикле равном количеству итераций
         for i in range(iter):
             print(i)
-            for batch_start in range(0, len(x), batch_size):
+            for batch_start in range(0, x.shape[0], batch_size):
                 # print(batch_start)
                 random_indices = np.random.choice(num_train, batch_size)
                 X_batch = x[random_indices]
                 y_batch = y[random_indices]
 
-                # X_batch = x
-                # y_batch = y
-
                 # метод сделает прогноз с текущими весами
                 y_pred = self.h(X_batch, thetas)
+
+
                 # найдет и запишет уровень ошибки
-                loss_history.append(self.objective(y_batch, y_pred))
+                loss = self.objective(y_batch, y_pred)
+                regularization_loss = (reg / (2 * n)) * np.sum(np.square(thetas[1:]))
+                loss += regularization_loss
+                loss_history.append(loss)
+
+
                 # рассчитает градиент
                 grad = self.gradient(X_batch, y_batch, y_pred, n)
+                regularization_grad = (reg / n) * thetas
+                regularization_grad[0] = 0
+                grad += regularization_grad
+
                 # и обновит веса
                 thetas -= learning_rate * grad
 
@@ -98,19 +104,20 @@ class LogReg():
         # добавит столбец из единиц
         self.add_ones(x)
         # рассчитает значения линейной функции
-        z = np.dot(x, self.thetas)
+        z = np.dot(x.toarray(), self.thetas)
         # передаст эти значения в сигмоиду
         probs = np.array([self.stable_sigmoid(value) for value in z])
         # выдаст принадлежность к определенному классу и соответствующую вероятность
         return np.where(probs >= 0.5, 1, 0), probs
 
     # ниже приводятся служебные методы, смысл которых был разобран ранее
-    def add_ones(self, x):
+    def add_ones(self, X):
         # return x.insert(0, 'x0', np.ones(x.shape[0]))
-        return np.c_[np.ones(x.shape[0]), x]
+        # return np.c_[np.ones(x.shape[0]), x]
+        return sparse.hstack((np.ones(X.shape[0])[:, np.newaxis], X)).tocsr()
 
     def h(self, x, thetas):
-        z = np.dot(x, thetas)
+        z = np.dot(x.toarray(), thetas)
         return np.array([self.stable_sigmoid(value) for value in z])
 
     def objective(self, y, y_pred):
@@ -119,7 +126,7 @@ class LogReg():
         return -np.mean(y_zero_loss + y_one_loss)
 
     def gradient(self, x, y, y_pred, n):
-        return np.dot(x.T, (y_pred - y)) / n
+        return np.dot(x.toarray().T, (y_pred - y)) / n
 
     def stable_sigmoid(self, z):
         if z >= 0:
@@ -130,48 +137,26 @@ class LogReg():
 
 # train_df = pd.read_csv('./data/train.csv')
 train_df = pd.read_csv('/home/soren/PycharmProjects/OTUS/10_numpy/homework/data/train.csv')
-train_df_pos = train_df[:10000]
-train_df_neg = train_df[-10000:]
-train_df = pd.concat([train_df_pos, train_df_neg])
+# train_df_pos = train_df[:40000]
+# train_df_neg = train_df[-40000:]
+# train_df = pd.concat([train_df_pos, train_df_neg])
 
-
-print(train_df.Prediction.value_counts(normalize=True))
-
-review_summaries = list(train_df['Reviews_Summary'].values)
+review_summaries = train_df['Reviews_Summary'].values.tolist()
 review_summaries = [l.lower() for l in review_summaries]
 
-print(review_summaries[:5])
-
-
 vectorizer = TfidfVectorizer()
-tfidfed = vectorizer.fit_transform(review_summaries)
-
-
-X = tfidfed.toarray()
+X = vectorizer.fit_transform(review_summaries)
 y = train_df.Prediction.values
-y_train: object
+
 X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.7, random_state=42)
 del X
 
 
-# # проверим работу написанного нами класса
-# # поместим признаки и целевую переменную в X и y
-# X = df[['alcohol', 'proline']]
-# y = df['target']
-#
-# # приведем признаки к одному масштабу
-# X = (X - X.mean()) / X.std()
-
 # создадим объект класса LogReg
-model = LogReg()
+model = LogisticRegression()
 
 # и обучим модель
-model.fit(X_train, y_train)
-
-
-# сделаем прогноз
-y_pred, probs = model.predict(X_train)
-print(accuracy_score(y_train, y_pred))
+model.train(X_train, y_train, num_iters=10)
 
 
 # сделаем прогноз on test
